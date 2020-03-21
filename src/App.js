faa8b675c490e42410b05fd1071f6f371f6271bc
EC2 Default User <ec2-user@ip-172-31-20-19.ec2.internal>
2020-03-21 04:50:05 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Ravinder with Docker.!!! Hi  there..
         </a>
       </header>
     </div>
